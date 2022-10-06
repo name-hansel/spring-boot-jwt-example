051bf4b9050b2f02974ad7bd7e992a1100edfc3a
Snehal Mane <snehal6mane@gmail.com>
2022-10-06 23:35:16 +0530

    Added custom filter

--- src/main/java/com/jwt/authexample/security/SecurityConfig.java
+++ src/main/java/com/jwt/authexample/security/SecurityConfig.java
@@ -1,12 +1,17 @@
 package com.jwt.authexample.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.jwt.authexample.filter.CustomAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
 
@@ -24,8 +29,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
-		super.configure(http);
+		http.csrf().disable();
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+		http.authorizeRequests().anyRequest().permitAll();
+		http.addFilter(new CustomAuthenticationFilter(authenticationManagerBean()));
+	}
+
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
 	}
 
 }
